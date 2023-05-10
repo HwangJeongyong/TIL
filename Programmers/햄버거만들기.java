488d06770c4caa5c91dc740a2cd792d56b033791
HwangJeongyong <jeongyong62@gmail.com>
2023-05-10 14:57:42 +0900

    2023/05/10 TIL commit

---
+++ Programmers/햄버거만들기.java
@@ -0,0 +1,23 @@
+// 2023/05/10
+// https://school.programmers.co.kr/learn/courses/30/lessons/133502
+package Programmers;
+
+public class 햄버거만들기 {
+    public static void main(String[] args) {
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
+    }
+    
+}
