6707d00bba1e2f331d67ff4760cf8b39f8f6f0ce
HwangJeongyong <jeongyong62@gmail.com>
2023-05-26 09:06:12 +0900

    2023/05/26 TIL commit

---
+++ Programmers/카드뭉치.java
@@ -0,0 +1,32 @@
+package Programmers;
+
+public class 카드뭉치 {
+    public static void main(String[] args) {
+        String[] cards1 = {"i", "drink", "water"};
+        String[] cards2 = {"want", "to"};
+        String[] goal = {"i", "want", "to", "drink", "water"};
+        String answer = "";
+        int c1 = 0;
+        int c2 = 0;
+        int g = 0;
+
+        for (int i = 0; i < goal.length; i++) {
+            if (c1 <= cards1.length - 1 && cards1[c1].equals(goal[i])) {
+                c1++;
+                g++;
+                continue;
+            }
+
+            if (c2 <= cards2.length - 1 && cards2[c2].equals(goal[i])) {
+                c2++;
+                g++;
+            }
+        }
+
+        if (g != goal.length) {
+            answer = "No";
+        } else {
+            answer = "Yes";
+        }
+    }
+}
