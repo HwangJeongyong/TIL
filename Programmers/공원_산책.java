542965f504c7cef7c72090a7d0c2866d28ddeb0c
HwangJeongyong <jeongyong62@gmail.com>
2023-05-27 17:16:06 +0900

    2023/05/27 TIL commit

--- Programmers/공원_산책.java
+++ Programmers/공원_산책.java
@@ -1,10 +1,66 @@
+// 2023/05/27
+// https://school.programmers.co.kr/learn/courses/30/lessons/172928
 package Programmers;
 
 public class 공원_산책 {
     public static void main(String[] args) {
         String[] park = {"SOO","OOO","OOO"};
         String[] routes = {"E 2","S 2","W 1"};
-        int[] answer = {};
-        
+        int[] result = {0, 0};
+        for (int i = 0; i < park.length; i++){
+            for (int j = 0; j < park[0].length(); j++){
+                if(park[i].charAt(j) == 'S'){
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        for (int i = 0; i < routes.length; i++){
+            String[] route = routes[i].split(" ");
+            int[] result2 = {result[0], result[1]};
+            boolean out = false;
+            for (int j = 0; j < Integer.parseInt(route[1]); j++){
+                int[] temp = {result[0], result[1]};
+                switch(route[0]){
+                    case "N":
+                        if (result[0] != 0){
+                            if (park[result[0] - 1].charAt(result[1]) != 'X'){
+                                result[0]--;
+                            }
+                        }
+                        break;
+                        case "S":
+                        if (result[0] != park.length - 1){
+                            if (park[result[0] + 1].charAt(result[1]) != 'X'){
+                                result[0]++;
+                            }
+                        }
+                        break;
+                        case "W":
+                        if (result[1] != 0){
+                            if (park[result[0]].charAt(result[1] - 1) != 'X'){
+                                result[1]--;
+                            }
+                        }
+                        break;
+                        case "E":
+                        if (result[1] != park[0].length() - 1){
+                            if (park[result[0]].charAt(result[1] + 1) != 'X'){
+                                result[1]++;
+                            }
+                        }
+                        break;
+                }
+                if (result[0] == temp[0] && result[1] == temp[1]) {
+                    out = true;
+                }
+            }
+
+            if (out){
+                result[0] = result2[0];
+                result[1] = result2[1];
+            }
+        }
     }
 }
