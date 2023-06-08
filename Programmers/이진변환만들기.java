d93a42f74e1a2b7665fe61c2d0b40861111764a9
HwangJeongyong <jeongyong62@gmail.com>
2023-06-09 08:31:56 +0900

    2023/06/09 TIL commit

--- Programmers/이진변환만들기.java
+++ Programmers/이진변환만들기.java
@@ -1,30 +1,33 @@
-// 
+// 2023/06/09
 // https://school.programmers.co.kr/learn/courses/30/lessons/70129
 package Programmers;
 
+import java.util.Arrays;
+
 public class 이진변환만들기 {
     public static void main(String[] args) {
-        String s = "110010101001";
+        String s = "01110";
         int[] answer = {0, 0};
         while(true){
+            answer[0]++;
             int dif = s.length() - s.replace("0", "").length();
             if (dif != 0) {
                 answer[1] += dif;
+                s = s.replace("0", "");
             }
-            answer[0]++;
-            s = s.replace("0", "");
             
             String s2 = "";
             int num = s.length();
             if (num == 1){
                 break;
             }
+            System.out.println("1 : " + s);
             int bi = 1;
-            while(num > bi) {
+            while(num >= bi) {
                 bi *= 2;
             }
             for (int i = bi/2; i > 0; i/=2){
-                if (num > i) {
+                if (num >= i) {
                     num -= i;
                     s2 += "1";
                 } else {
@@ -33,8 +36,10 @@ public class 이진변환만들기 {
             }
 
             s = s2;
+            System.out.println("2 : " + s);
         }
-
-
+        System.out.println(Arrays.toString(answer));
+        
+        
     }
 }
